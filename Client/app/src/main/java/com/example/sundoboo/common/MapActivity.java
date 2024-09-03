66950170d3ce7f4e97c2c042f8304c44d4aa048e
dbwjdrbs <klolp2@naver.com>
2024-09-03 12:27:43 +0900

    Design: 버튼 색상이 보라색으로 고정되는 문제 해결

---
+++ Client/app/src/main/java/com/example/sundoboo/common/MapActivity.java
@@ -0,0 +1,16 @@
+package com.example.sundoboo.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.client.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
