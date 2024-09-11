741021fae77d0fa9db3e2a6ec23d9c1f0913223b
seoyeon1221 <rr124356@naver.com>
2024-09-11 09:43:39 +0900

    수정ver2

--- Server/src/main/java/com/springboot/businessscore/service/BusinessScoreService.java
+++ Server/src/main/java/com/springboot/businessscore/service/BusinessScoreService.java
@@ -5,10 +5,14 @@ import com.springboot.business.service.BusinessService;
 import com.springboot.businessscore.dto.BusinessScoreDto;
 import com.springboot.businessscore.entity.BusinessScore;
 import com.springboot.businessscore.repository.BusinessScoreRepository;
+import com.springboot.exception.BusinessLogicException;
+import com.springboot.exception.ExceptionCode;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Service
@@ -23,7 +27,9 @@ public class BusinessScoreService {
     }
 
     public BusinessScore createBusinessScore(BusinessScoreDto.Post requestBody) {
+        // 검증 로직 추가 가능
         Business findBusiness = businessService.verifyExistBusiness(requestBody.getBusinessId());
+        verifyBusinessScoreTitle(requestBody.getBusinessScoreTitle());
 
         BusinessScore businessScore = new BusinessScore();
         businessScore.setBusiness(findBusiness);
@@ -34,7 +40,7 @@ public class BusinessScoreService {
         businessScore.setScoreList4(requestBody.getScoreList4());
         businessScore.setObserverName(requestBody.getObserverName());
 
-       // 검증 로직 추가 가능
+
 
         return businessScoreRepository.save(businessScore);
 
@@ -49,12 +55,12 @@ public class BusinessScoreService {
 //    }
 
 //  사업에 대한 평가를 평가명으로 한정할 경우.
-//    public void verifyBusinessScoreTitle(String businessScoreTitle){
-//        Optional<BusinessScore> optionalBusinessScore = businessScoreRepository.findByBusinessScoreTitle(businessScoreTitle);
-//        optionalBusinessScore.ifPresent(BusinessScore -> {
-//            throw new BusinessLogicException(ExceptionCode.BUSINESS_SCORE_TITLE_ALREADY_EXISTS);
-//        });
-//    }
+    public void verifyBusinessScoreTitle(String businessScoreTitle){
+        Optional<BusinessScore> optionalBusinessScore = businessScoreRepository.findByBusinessScoreTitle(businessScoreTitle);
+        optionalBusinessScore.ifPresent(BusinessScore -> {
+            throw new BusinessLogicException(ExceptionCode.BUSINESS_SCORE_TITLE_ALREADY_EXISTS);
+        });
+    }
 
     // 빈데이터 응답 처리기 때문에 없는 아이디 입력하면 빈화면 나와서 따로 예외 안던짐
     public Page<BusinessScore> findScore(long businessId, int page, int size){
--- Server/src/main/java/com/springboot/exception/ExceptionCode.java
+++ Server/src/main/java/com/springboot/exception/ExceptionCode.java
@@ -9,8 +9,6 @@ public enum ExceptionCode {
     BUSINESS_SCORE_TITLE_ALREADY_EXISTS(404, "BusinessScore title already exists"),
     TURBINE_NOT_FOUND(404, "Turbine not found"),
     LOCATION_NOT_FOUND(404, "Location not found"),
-    BUSINESS_TITLE_NOT_NULL(400, "BusinessTitle NotNull"),
-    BUSINESS_TITLE_TOO_LONG(400, "BusinessTitle must not exceed 20 characters"),
     BUSINESS_ALREADY_EXISTS(404,"Business Title already exists");
     @Getter
     private int status;
